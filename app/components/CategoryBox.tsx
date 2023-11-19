7849020dfccddb5cea2e6ffae623bebfca329afd
hall-e3 <hallenochasanda@gmail.com>
2023-11-19 10:24:39 +0300

    working on categories, query string installation

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -18,10 +18,29 @@ const CategoryBox = ({ icon: Icon, label, selected }: CategoryBoxProps) => {
     if (params) {
       currentQuery = qs.parse(params.toString());
     }
-  }, []);
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+    // if the category we click on has already been selected, we delete from the query object
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+    router.push(url);
+  }, [label, params, router]);
 
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
         selected
           ? "border-b-neutral-800 text-neutral-800"
